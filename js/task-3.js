9ee732f78bba4d1062addc85adac4492e3ed3f9f
nuridze <113994431+nuridze@users.noreply.github.com>
2023-12-06 19:38:32 +0100

    1

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
